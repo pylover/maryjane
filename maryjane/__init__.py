34d234faec80123b7bcc6304f869e6e6fc91faf9
vahid <vahid@dobisel.com>
2016-10-09 16:29:44 +0330

    version flag

--- maryjane/__init__.py
+++ maryjane/__init__.py
@@ -1,24 +1,27 @@
-
 from .core import Project
 from watchdog.observers import Observer
 import argparse
-import sys
 
-
-__version__ = '4.0.0'
-
+__version__ = '4.0.0-dev0'
 
 parser = argparse.ArgumentParser(description='File watcher and task manager')
 parser.add_argument('manifest', nargs='?', default='maryjane.yaml', help='Manifest file, default: "maryjane.yaml"')
-parser.add_argument('-w','--watch', dest='watch', action='store_true', help='Watch for modifications, and execute tasks if needed.')
+parser.add_argument('-w', '--watch', dest='watch', action='store_true',
+                    help='Watch for modifications, and execute tasks if needed.')
+parser.add_argument('-V', '--version', dest='version', action='store_true', help='Show version.')
 
 
 def main():
     args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        return
+
     p = Project(args.manifest, watcher_type=Observer if args.watch else None)
     if args.watch:
         try:
-            sys.exit(p.wait_for_changes())
+            return p.wait_for_changes()
         except KeyboardInterrupt:
             print('CTRL+C detected.')
-            sys.exit(1)
+            return 1
