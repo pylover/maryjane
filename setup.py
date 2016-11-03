54c2dbe6d0a84b2eae913df054fad9055327c4a8
Vahid <vahid.mardani@gmail.com>
2016-11-03 11:53:24 +0330

    Closes #15

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 
-from setuptools import setup
-import os.path
+import sys
 import re
+import os.path
+from setuptools import setup
+
 
 # reading package's version (same way sqlalchemy does)
 with open(os.path.join(os.path.dirname(__file__), 'maryjane.py')) as v_file:
     package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+if sys.version_info < (3, 6, 0):
+    print('Error: `Python < 3.6` is not supported.', file=sys.stdout)
+    sys.exit(-1)
+
 
 long_description = """
 Simple Task runner and build tool.
